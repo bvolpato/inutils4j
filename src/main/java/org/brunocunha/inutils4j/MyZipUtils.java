6149f1bfce46e594b52b2d052b19ad733a9dee1a
Bruno Candido Volpato da Cunha <brunocvcunha@gmail.com>
2015-06-11 10:46:55 -0300

    adding zip utilities

---
+++ src/main/java/org/brunocunha/inutils4j/MyZipUtils.java
@@ -0,0 +1,150 @@
+package org.brunocunha.inutils4j;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class MyZipUtils {
+
+	/**
+	 * Extracts the zip file to the output folder
+	 * 
+	 * @param zipFile
+	 *            ZIP File to extract
+	 * @param outputFolder
+	 *            Output Folder
+	 * @return A Collection with the extracted files
+	 * @throws IOException
+	 */
+	public static List<File> extract(File zipFile, File outputFolder)
+			throws IOException {
+		List<File> extracted = new ArrayList<File>();
+
+		byte[] buffer = new byte[2048];
+
+		if (!outputFolder.exists()) {
+			outputFolder.mkdir();
+		}
+
+		ZipInputStream zipInput = new ZipInputStream(new FileInputStream(
+				zipFile));
+
+		ZipEntry zipEntry = zipInput.getNextEntry();
+
+		while (zipEntry != null) {
+
+			String neFileNameName = zipEntry.getName();
+			File newFile = new File(outputFolder + File.separator
+					+ neFileNameName);
+
+			newFile.getParentFile().mkdirs();
+
+			if (!zipEntry.isDirectory()) {
+				FileOutputStream fos = new FileOutputStream(newFile);
+
+				int size;
+				while ((size = zipInput.read(buffer)) > 0) {
+					fos.write(buffer, 0, size);
+				}
+
+				fos.close();
+				extracted.add(newFile);
+			}
+
+			zipEntry = zipInput.getNextEntry();
+		}
+
+		zipInput.closeEntry();
+		zipInput.close();
+
+		return extracted;
+
+	}
+	
+	
+
+	/**
+	 * Checks if a Zip is valid navigating through the entries
+	 * 
+	 * @param file
+	 * @throws IOException
+	 */
+	public static void validateZip(File file) throws IOException {
+		ZipInputStream zipInput = new ZipInputStream(new FileInputStream(file));
+		ZipEntry zipEntry = zipInput.getNextEntry();
+
+		while (zipEntry != null) {
+			zipEntry = zipInput.getNextEntry();
+		}
+
+		try {
+			if (zipInput != null) {
+				zipInput.close();
+			}
+		} catch (IOException e) {
+		}
+	}
+	
+	/**
+	 * Compress a directory into a zip file
+	 * @param dir
+	 * @param zipFile
+	 * @throws IOException
+	 */
+	public static void compress(File dir, File zipFile) throws IOException {
+
+		FileOutputStream fos = new FileOutputStream(zipFile);
+		ZipOutputStream zos = new ZipOutputStream(fos);
+
+		recursiveAddZip(dir, zos, dir);
+
+		zos.finish();
+		zos.close();
+
+	}
+
+	/**
+	 * Recursively add files to a ZipOutputStream 
+	 * @param parent
+	 * @param zout
+	 * @param fileSource
+	 * @throws IOException
+	 */
+	public static void recursiveAddZip(File parent, ZipOutputStream zout,
+			File fileSource) throws IOException {
+
+		File[] files = fileSource.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].isDirectory()) {
+				recursiveAddZip(parent, zout, files[i]);
+				continue;
+			}
+
+			byte[] buffer = new byte[1024];
+
+			FileInputStream fin = new FileInputStream(files[i]);
+
+			ZipEntry zipEntry = new ZipEntry(files[i].getAbsolutePath()
+					.replace(parent.getAbsolutePath(), "").substring(1)); //$NON-NLS-1$
+			zout.putNextEntry(zipEntry);
+
+			int length;
+			while ((length = fin.read(buffer)) > 0) {
+				zout.write(buffer, 0, length);
+			}
+
+			zout.closeEntry();
+
+			fin.close();
+
+		}
+
+	}
+}
